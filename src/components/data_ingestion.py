f2a4cfd18def56e1771ea2ed4617691601f14249
Diego Cotacio <diego.cotacio@correounivalle.edu.co>
2023-07-25 10:07:27 -0500

    fixed import

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,5 +8,46 @@ from dataclasses import dataclass
 
 from src.exception import CustomException
 from src.logger import logging
-from src.components.data_preprocessing import DataTransformation, DataTransformationConfig
-from src.components.model_trainer import ModelTrainer, ModelTrainingConfig
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacs', "data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Start data ingestion")
+
+        try:
+            df = pd.read_csv("data/dataset.csv")
+            logging.info("Load the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok= True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header= True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+#test local component
+if __name__=="__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
